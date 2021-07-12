cf395fec6891ce66cf1016c3b33193bc990ec89c
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-13 02:18:35 +0530

    added: an array and stored the daily wage in it

--- empWage.js
+++ empWage.js
@@ -9,15 +9,14 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 10;
-const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 
 /**
- * calculating the wage of an employee till
- * the condition of total working hrs or number of working days
- * has been reached to maximum limit
- * @param {*} empCheck : checking employee is present is absent
- * @returns employee working hours
+ * This function is use to check working hours of the employee
+ * who is present and for absent it is zero
+ * @param {*} empCheck : checking employee is present or absent
+ * @returns working hours of an employee
  */
 function getWorkingHours(empCheck) {
   switch (empCheck) {
@@ -29,17 +28,34 @@ function getWorkingHours(empCheck) {
       return 0;
   }
 }
+/**
+ * calculating the daily wage and returns it
+ * @param {*} empHrs : daily working hrs
+ * @returns  daily wage of an employee
+ */
+function calculateDailyWage(empHrs){
+    return empHrs*WAGE_PER_HOUR;
+}
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+// creating an array to store the daily wage of an employee
+let empDailyWageArr = new Array();
+
 while (
   totalEmpHrs <= MAX_HRS_IN_MONTH &&
   totalWorkingDays < NUM_OF_WORKING_DAYS
 ) {
   totalWorkingDays++;
   let empCheck = Math.floor(Math.random() * 10) % 3;
-  totalEmpHrs += getWorkingHours(empCheck);
+  let empHrs = getWorkingHours(empCheck);
+  totalEmpHrs += empHrs;
+  //pushing the daily wage to an array
+  empDailyWageArr.push(calculateDailyWage(empHrs));
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+/**
+ * calculating the employee wage on the total working hours
+ */
+let empWage = calculateDailyWage(totalEmpHrs);
 console.log(
   "Total Days: " +
     totalWorkingDays +
