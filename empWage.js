be13493c305b2ae6b45fea270f8766302869abbb
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-13 00:23:23 +0530

    added: monthly wage of an employee

--- empWage.js
+++ empWage.js
@@ -20,17 +20,17 @@
 
 //UC2 SWITCH CASE
 
-  const IS_PART_TIME = 1;
-  const IS_FULL_TIME = 2;
-  const PART_TIME_HOURS = 4;
-  const FULL_TIME_HOURS = 8;
-  const WAGE_PER_HOUR = 20;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 /**
- * UC3
- * calculated daily wage of employee using function
- * @param {*} empCheck 
- * @returns empHrs
+ * calculating monthly wage of employee using function
+ * @param {*} empCheck
+ * @returns employee working hours
  */
 function getWorkingHours(empCheck) {
   switch (empCheck) {
@@ -42,8 +42,10 @@ function getWorkingHours(empCheck) {
       return 0;
   }
 }
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Hours :" + empHrs + " Employee wage is :" + empWage);
+let totalEmpHrs = 0;
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+  let empCheck = Math.floor(Math.random() * 10) % 3;
+  totalEmpHrs += getWorkingHours(empCheck);
+}
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Hours :" + totalEmpHrs + " Employee wage is :" + empWage);
