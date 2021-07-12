17f7c3a925818ad4d715b6a7b23de31052e6f80b
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-13 00:42:28 +0530

    added: calculated total wages till maximum days or maximum working hrs

--- empWage.js
+++ empWage.js
@@ -3,33 +3,20 @@
  * @author Rosy Rupali
  * @since 12-07-2021
  *****************************************************/
-/**
- * UC1 - IF ELSE CONDITION
- * To check employee is present or absent
- */
-{
-  const IS_ABSENT = 0;
-  let empCheck = Math.floor(Math.random() * 10) % 2;
-  if (empCheck == IS_ABSENT) {
-    console.log("Employee is Absent.");
-    return;
-  } else {
-    console.log("Employee is present");
-  }
-}
-
-//UC2 SWITCH CASE
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 20;
+const NUM_OF_WORKING_DAYS = 10;
+const MAX_HRS_IN_MONTH = 100;
 
 /**
- * calculating monthly wage of employee using function
- * @param {*} empCheck
+ * calculating the wage of an employee till
+ * the condition of total working hrs or number of working days
+ * has been reached to maximum limit
+ * @param {*} empCheck : checking employee is present is absent
  * @returns employee working hours
  */
 function getWorkingHours(empCheck) {
@@ -43,9 +30,21 @@ function getWorkingHours(empCheck) {
   }
 }
 let totalEmpHrs = 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+let totalWorkingDays = 0;
+while (
+  totalEmpHrs <= MAX_HRS_IN_MONTH &&
+  totalWorkingDays < NUM_OF_WORKING_DAYS
+) {
+  totalWorkingDays++;
   let empCheck = Math.floor(Math.random() * 10) % 3;
   totalEmpHrs += getWorkingHours(empCheck);
 }
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("Hours :" + totalEmpHrs + " Employee wage is :" + empWage);
+console.log(
+  "Total Days: " +
+    totalWorkingDays +
+    "Total Hours :" +
+    totalEmpHrs +
+    " Employee wage is :" +
+    empWage
+);
